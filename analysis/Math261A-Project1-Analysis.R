7750dfb38979e982f3f8857d415defe9a0c42602
AndrewBruceYoung <andrew.y9000@gmail.com>
2025-09-23 17:09:48 -0700

    Further data cleaning, creating table I will work with"

--- analysis/Math261A-Project1-Analysis.R
+++ analysis/Math261A-Project1-Analysis.R
@@ -5,10 +5,17 @@ library(ggplot2)
 
 # reading file
 
-df <- read_csv("data/2025-registered-nurse-shortage-areas-raw.csv" , show_col_types = FALSE, trim_ws = TRUE) |>
-  clean_names() |>
-  remove_empty("cols") |>
+df <- read_csv("data/2025-registered-nurse-shortage-areas-raw.csv") %>%
+  clean_names() %>%
+  remove_empty("cols") %>%
   mutate(
     gap_pct        = readr::parse_number(gap_percent),
     est_population = as.numeric(est_population)
   )
+
+# keeping important columns
+cleaned_data <- df %>%
+  select(county, est_population, gap_pct, severity, rnsa_designation) %>%
+  filter(is.finite(est_population), is.finite(gap_pct))
+
+glimpse(cleaned_data)
