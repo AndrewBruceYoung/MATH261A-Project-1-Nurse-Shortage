db9580b974247011514ff0c976634be1558f0e2c
AndrewBruceYoung <andrew.y9000@gmail.com>
2025-09-23 17:26:06 -0700

    Created linear model

--- analysis/Math261A-Project1-Analysis.R
+++ analysis/Math261A-Project1-Analysis.R
@@ -4,7 +4,6 @@ library(janitor)
 library(ggplot2)
 
 # reading file
-
 df <- read_csv("data/2025-registered-nurse-shortage-areas-raw.csv") %>%
   clean_names() %>%
   remove_empty("cols") %>%
@@ -18,4 +17,7 @@ cleaned_data <- df %>%
   select(county, est_population, gap_pct, severity, rnsa_designation) %>%
   filter(is.finite(est_population), is.finite(gap_pct))
 
-glimpse(cleaned_data)
+# create linear model
+m1 <- lm(gap_pct ~ est_population, data = cleaned_data)
+summary(m1)
+glance(m1)
