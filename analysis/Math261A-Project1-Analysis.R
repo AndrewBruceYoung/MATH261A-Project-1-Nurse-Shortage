9f1f7724e527d43cfbe45d9b69aa48f37eb6ecf6
AndrewBruceYoung <andrew.y9000@gmail.com>
2025-10-29 21:40:48 -0700

    Updated to follow professor recommendations

--- analysis/Math261A-Project1-Analysis.R
+++ analysis/Math261A-Project1-Analysis.R
@@ -2,41 +2,39 @@ library(readr)
 library(dplyr)
 library(janitor)
 library(ggplot2)
+library(knitr)
+library(scales)
 
-# reading file
-df <- read_csv("data/2025-registered-nurse-shortage-areas-raw.csv") %>%
-  clean_names() %>%
-  remove_empty("cols") %>%
-  mutate(
-    gap_pct        = readr::parse_number(gap_percent),
-    est_population = as.numeric(est_population)
-  )
+#Reading and cleaning
+cleaned_data <- read_csv("2025-registered-nurse-shortage-areas-cleaned.csv", show_col_types = FALSE) %>%
+  clean_names()
 
-# keeping important columns
-cleaned_data <- df %>%
-  select(county, est_population, gap_pct, severity, rnsa_designation) %>%
-  filter(is.finite(est_population), is.finite(gap_pct))
+cleaned_data %>%
+  summarise(
+    n_areas = n(),
+    pop_min = min(est_population, na.rm = TRUE),
+    pop_median = median(est_population, na.rm = TRUE),
+    pop_max = max(est_population, na.rm = TRUE),
+    gap_min_pct = min(gap_pct, na.rm = TRUE),
+    gap_median = median(gap_pct, na.rm = TRUE),
+    gap_max_pct = max(gap_pct, na.rm = TRUE)) %>%
+  kable(caption = "Sample size and ranges for population and RN gap percentage.")
 
 # create linear model
-m1 <- lm(gap_pct ~ est_population, data = cleaned_data)
-summary(m1)
+dm1 <- lm(gap_pct ~ est_population, data = cleaned_data)
+summary(dm1)
 
 # plot scatter plot along with linear regression line
 ggplot(cleaned_data, aes(x = est_population, y = gap_pct)) +
   geom_point(alpha = 0.85) +
   geom_smooth(method = "lm", se = TRUE) +
-  scale_x_continuous(
-    labels = scales::label_number(scale_cut = scales::cut_short_scale())
-  ) +
-  labs(
-    title = "Projected RN Shortage Gap Percentage vs Population",
-    x = "Population",
-    y = "RN Gap Percent"
-  ) +
+  scale_x_continuous(labels = label_number(scale_cut = cut_short_scale())) +
+  labs(x = "Population", y = "RN Gap (%)") +
   theme_minimal()
 
 # Diagnostics
-par(mfrow = c(1,2))
-plot(m1, which = 1)
-plot(m1, which = 2)
 par(mfrow = c(1,1))
+plot(dm1, which = 1, sub.caption = "", caption = "Residuals vs Fitted")
+
+par(mfrow = c(1,1))
+plot(dm1, which = 2, sub.caption = "", caption = "Normal Q–Q")
